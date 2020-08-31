2eef383c198f19805b025b547e8b1251fe33102a
Hocheol <samiosae@gmail.com>
2020-08-31 15:52:27 +0900

    Fix route error /카드넘버 로 구성하던 것에서 /main/카드넘버 로 변경하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Intro from './pages/Intro';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Userinfo from './pages/Userinfo';
-import Main from './pages/Main';
-import Mypage from './pages/Mypage';
-import Upload from './pages/Upload';
-import NotFound from './pages/NotFound';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Intro from "./pages/Intro";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Userinfo from "./pages/Userinfo";
+import Main from "./pages/Main";
+import Mypage from "./pages/Mypage";
+import Upload from "./pages/Upload";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/login"><Login /></Route>
-        <Route exact path="/signup"><Signup /></Route>
-        <Route exact path="/userinfo"><Userinfo /></Route>
-        <Route exact path="/:id" component={Main} />
-        <Route exact path="/mypage"><Mypage /></Route>
-        <Route exact path="/upload"><Upload /></Route>
-        <Route exact path="/"><Intro /></Route>
-        <Route><NotFound /></Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/userinfo">
+          <Userinfo />
+        </Route>
+        <Route exact path="/mypage">
+          <Mypage />
+        </Route>
+        <Route exact path="/upload">
+          <Upload />
+        </Route>
+        <Route exact path="/main/:id" component={Main} />
+        <Route exact path="/">
+          <Intro />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
