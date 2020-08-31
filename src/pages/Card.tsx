007ad111e00565ce05a1e17a6591ca2546dc2f7e
Angelo <bbirds94@gmail.com>
2020-09-01 01:08:39 +0900

    [Implement] Mainpage view
     - 메인페지이의 간략 뷰를 구현했습니다.

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import "./Card.css"
+import Comment from './Comment'
 function Card() {
     let { id } = useParams();
-    return <div>{id}의 카드입니다.</div>
+    return (
+        <>
+            <div className="card_box">
+                <div className="card">
+                    {id}의 카드입니다.
+                asdsdasd
+                asdasdasd
+            </div>
+                <Comment></Comment>
+            </div>
+            <div className="card_box">
+                <div className="card">
+                    {id}의 카드입니다.
+                asdsdasd
+                asdasdasd
+            </div>
+                <Comment></Comment>
+            </div>
+            <div className="card_box">
+                <div className="card">
+                    {id}의 카드입니다.
+                asdsdasd
+                asdasdasd
+            </div>
+                <Comment></Comment>
+            </div>
+        </>
+    )
 }
 
 export default Card;
