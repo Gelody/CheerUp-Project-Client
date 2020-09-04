bb450c33aea264fb4c293643a0661e50319b11b9
Angelo <bbirds94@gmail.com>
2020-09-04 13:44:12 +0900

    [Update]Mypage

--- src/pages/MycardList.tsx
+++ src/pages/MycardList.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import './MycardList.css'
-import axios from 'axios';
 
-function MycardList() {
-    const user = JSON.parse(window.sessionStorage.user);
-    let A: any = [];
-
-    axios
-        .get('/card/get', { headers: { authorization: user } })
-        .then((res) => {
-            if (res.status === 200) {
-                console.log(res.data);
-                console.log("카드의 정보를 성공적으로 가져왔습니다")
-                A.push(res.data);
-            } else {
-                console.log("카드의 정보를 가져오는데 문제가 있습니다")
-            }
-        })
-        .catch(err => console.log(err))
-
+function MycardList(props: any) {
+    console.log(props)
+    let list = props
+    console.log(list)
     return (
         <>
             <div className="mycardlist_wrap">
                 받아온 데이터를 이곳에 보여주고 싶습니다.
-                {console.log(A)}
-                {console.log(A)}
-                {console.log(A)}
-                {A}
                 <form>
                     <button className="review_button" type="submit">후기등록</button>
                 </form>
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Dashboard from "./Dashboard";
 import MycardList from "./MycardList";
 import CheeringList from "./CheeringList";
 import Nav from "./Nav";
+import axios from "axios";
 
 function Mypage() {
+  const [mycard, setmycard]: any = useState([])
+  const user = JSON.parse(window.sessionStorage.user);
+
+  axios
+    .get('/card/get', { headers: { authorization: user } })
+    .then((res) => {
+      if (res.status === 200) {
+        console.log("카드의 정보를 성공적으로 가져왔습니다")
+        mycard.push(res.data);
+        // console.log(mycard)
+      } else {
+        console.log("카드의 정보를 가져오는데 문제가 있습니다")
+      }
+    })
+    .catch(err => console.log(err))
+
   return (
     <>
       <Nav></Nav>
       <Dashboard></Dashboard>
-      <MycardList></MycardList>
+      <MycardList {...mycard} />
       <CheeringList></CheeringList>
     </>
   );
