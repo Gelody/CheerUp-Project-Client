44fc2e58bd471ee23208327ad16bbb48e3ef02a3
Angelo <bbirds94@gmail.com>
2020-09-04 23:32:31 +0900

    [BugFix] Mypage rendering bug fix
     - useState와 useEffect를 적용하니 해결되었습니다

--- src/pages/MycardList.tsx
+++ src/pages/MycardList.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import './MycardList.css';
-import './Mypage';
-// ["ㅁㄴㅇㅁㄴㅇ", "ㅁㅇㅁㄴㅇ", "sasdasd"]
-function MycardList({ mycards }: any) {
-    console.log("여기는 카드리스트", mycards)
 
-    const cardList = <ul>{mycards.map((mycard: string) => <li>{mycard}</li>)}</ul>
-    // console.log(mycard)
-    console.log("return하기전 카드리스트", cardList);
+function MycardList({ mycard }: any) {
     return (
         <>
             <div className="mycardlist_wrap">
-                프롭스를 리스트로 보여주면됩니다.
-                나오나?
-                {console.log("카드리스트", cardList)}
-                {cardList}
-                <form>
-                    <button className="review_button" type="submit">후기등록</button>
-                </form>
+                {mycard.map((card: any, index: any) => (
+                    <div key={index}>
+                        <div>{card.text}</div>
+                        <form>
+                            <button className="review_button" type="submit">후기등록</button>
+                        </form>
+                    </div>
+                ))}
             </div>
         </>
     )
