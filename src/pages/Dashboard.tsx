8577342c16b23deeaa555f3c42034405bf4e2a97
Hocheol <samiosae@gmail.com>
2020-09-07 13:51:50 +0900

    [Feature] Implement SignOut 마이페이지에 있는 로그아웃 버튼을 클릭하면 세션스토리지에 저장된 토큰을 clear하고 홈으로 이동합니다.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import './Dashboard.css'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./Dashboard.css";
 
 function Dashboard() {
-    return (
-        <>
-            <div className="dashborad_wrap">
-                <h1>대시보드</h1>
-                <span>당신의 응원레벨</span>
-                <span>LV.22</span>
-                <br />
-                <li>총 받은 응원 수 누적데이터
-                <p>지금까지 ~</p>
-                </li>
-                <li>총 응원 한 수 누적데이터
-                <p>지금까지 ~</p>
-                </li>
-            </div>
-        </>
-    )
+  const history = useHistory();
+  return (
+    <>
+      <div className="dashborad_wrap">
+        <h1>대시보드</h1>
+        <span>당신의 응원레벨</span>
+        <span>LV.22</span>
+        <button
+          onClick={() => {
+            sessionStorage.clear();
+            alert("성공적으로 로그아웃 되었습니다.");
+            history.push("/");
+          }}
+        >
+          로그아웃
+        </button>
+        <br />
+        <li>
+          총 받은 응원 수 누적데이터
+          <p>지금까지 ~</p>
+        </li>
+        <li>
+          총 응원 한 수 누적데이터
+          <p>지금까지 ~</p>
+        </li>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
