72b7445ffc27ffcd933b97c17f51041e56d66034
bbirds94 <bbirds94@hotmail.com>
2020-09-16 22:56:36 +0900

    [Update] MainPage Review  Rendering

--- src/pages/Mainpage/Main.tsx
+++ src/pages/Mainpage/Main.tsx
@@ -3,23 +3,22 @@ import Card from "./Card";
 import Nav from "../Nav";
 import axios from "axios";
 
-
 // 메인페이지, 모든 카드 받아오는 요청
 function Main() {
-  const [cards, setCards]: any = useState([])
+  const [cards, setCards]: any = useState([]);
 
   useEffect(() => {
     axios
-      .get('/card/getAll')
+      .get("/card/getAll")
       .then(({ data }) => {
         setCards(data);
-        // if (data) {
-        //   console.log("모든 카드를 성공적으로 가져왔습니다")
-        // } else {
-        //   console.log("카드 데이터가 없습니다")
-        // }
+        if (data) {
+          console.log("모든 카드를 성공적으로 가져왔습니다", data);
+        } else {
+          console.log("카드 데이터가 없습니다");
+        }
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }, []);
 
   return (
--- src/pages/UploadCard.tsx
+++ src/pages/UploadCard.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./UploadCard.css";
 import axios from "axios";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 // 카드 업로드 기능
 function UploadCard() {
   const [text, setText] = useState("");
+  const [date, setDate] = useState(new Date());
   const user = JSON.parse(window.sessionStorage.user);
   const cardData = { text: text, user_Id: "user" };
+
+  // 달력
+  const onChangeDate = () => {
+    setDate(date);
+  };
 
   // 카드 등록 요청
   const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
@@ -36,8 +44,16 @@ function UploadCard() {
             value={text}
             onChange={onChangeText}
             maxLength={300}
-            placeholder="어떤 일에 응원이 필요하신가요?"
+            placeholder="Step1. 어떤 일에 응원이 필요하신가요?"
           ></textarea>
+          <p className="calendar_title">
+            Step.2 언제까지 응원이 필요하신지 선택 해 주세요
+          </p>
+          <Calendar
+            className="calendar"
+            onChange={onChangeDate}
+            value={date}
+          ></Calendar>
           <button className="upload_button" type="submit">
             카드 등록하기
           </button>
