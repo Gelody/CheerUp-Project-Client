407525516c391dfa028f081b0ae3dfc85efa10cc
bbirds94 <bbirds94@hotmail.com>
2020-09-21 13:34:38 +0900

    [Update] modal card css

--- src/pages/Mypage/Dashboard.tsx
+++ src/pages/Mypage/Dashboard.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
 import "./Dashboard.css";
 import swal from "sweetalert";
 
 // 마이페이지에서의 대쉬보드(응원 받거나 한 숫자 카운트, 로그아웃)
 function Dashboard({ cheeringCount, cheeredCount }: any) {
+  const [userInfo, setUserInfo]: any = useState([]);
+  const user = JSON.parse(window.sessionStorage.user);
   const history = useHistory();
   const count = cheeringCount[0]?.Commentcount;
   const level = (count: any) => {
@@ -17,11 +20,27 @@ function Dashboard({ cheeringCount, cheeredCount }: any) {
     }
   };
 
+  // 로그인한 유저의 id 요청
+  useEffect(() => {
+    axios
+      .get("/user/getid", { headers: { authorization: user } })
+      .then(({ data }) => {
+        setUserInfo(data);
+        if (data) {
+          console.log("유저 아이디를 성공적으로 받았습니다", data);
+        } else {
+          console.log("유저의 아이디 데이터가 없습니다");
+        }
+      });
+    // .catch((err) => console.log(err));
+  }, []);
+
+  console.log("유저네임", userInfo.userName);
   return (
     <>
       <div className="dashborad_wrap">
         <h1>대시보드</h1>
-        <span>당신의 응원레벨</span>
+        <span>{userInfo.userName}의 응원레벨</span>
         <span>LV.{level(count)}</span>
         <button
           onClick={() => {
